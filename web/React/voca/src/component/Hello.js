dc4ae21339adb788def29fba47e34458165c1d86
Seunghun <shlee4508@gmailcom>
2024-03-13 15:34:54 +0900

    오브젝트

--- web/React/voca/src/component/Hello.js
+++ web/React/voca/src/component/Hello.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-export default function Hello(props) {
+export default function Hello({age, height}) {
     // props = read only json으로 넘어옴
 
     // useState 는 배열을 반환함, 첫번째 값은 state이며 두번째는 state를 변경해주는 함수이다.
     const [name, setName] = useState("Mike");
-    const [age, setAge] = useState(props.age);
+    const [Age, setAge] = useState(age);
 
     //let name = "Mike"
     function changeName() {
@@ -15,14 +15,15 @@ export default function Hello(props) {
         //document.getElementById("name").innerText = name;
 
         setName(name === "Mike" ? "Jane" : "Mike");
-        setAge(age + 1);
+        setAge(Age + 1);
     }
 
     return (
         <div>
             <h1>state</h1>
             <h2>컴포넌트 속성값</h2>
-            <h2>{name}({age}세)</h2>
+            <h2>{name}({Age}세)(키 {height})</h2>
+
             <button onClick={changeName}>Change</button>
         </div>
     );
