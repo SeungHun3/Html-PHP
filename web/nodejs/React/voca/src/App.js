4cd78370f9c5720a2943692c0b47fa0947fd7d74
Seunghun <shlee4508@gmailcom>
2024-03-13 13:01:59 +0900

    리액트 프로젝트 설정

---
+++ web/nodejs/React/voca/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  const name = "Seunghun";
+  const naver = 
+  {
+    name: "네이버",
+    url: "https://naver.com",
+  }
+  return (
+    <div className="App">
+      <h1
+        style={{
+          color: "#f0f",
+          backgroundColor: "green",
+        }}
+      >
+        hello {name}
+      </h1>
+      <a href={naver.url}> {naver.name}</a>
+    </div>
+  );
+}
+
+export default App;
