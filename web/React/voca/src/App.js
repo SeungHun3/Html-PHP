f7677b3de8d48653db019250de2041c01b51e482
Seunghun <shlee4508@gmailcom>
2024-03-13 15:32:32 +0900

    props

--- web/React/voca/src/App.js
+++ web/React/voca/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import Hello from './component/Hello';
-import Welcome from './component/Welcome';
-import styles from "./App.module.css";
 function App() {
 
   const name = "Seunghun";
@@ -13,9 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <Hello />
-      <Hello />
-      <Hello />
+      <Hello age = {10} height = {160}/>
     </div>
   );
 }
--- web/React/voca/src/component/Hello.js
+++ web/React/voca/src/component/Hello.js
@@ -1,8 +1,12 @@
 import { useState } from "react";
 
-export default function Hello() {
+export default function Hello(props) {
+    // props = read only json으로 넘어옴
+
     // useState 는 배열을 반환함, 첫번째 값은 state이며 두번째는 state를 변경해주는 함수이다.
     const [name, setName] = useState("Mike");
+    const [age, setAge] = useState(props.age);
+
     //let name = "Mike"
     function changeName() {
         //name = name === "Mike" ? "Jane" : "Mike";
@@ -11,13 +15,14 @@ export default function Hello() {
         //document.getElementById("name").innerText = name;
 
         setName(name === "Mike" ? "Jane" : "Mike");
+        setAge(age + 1);
     }
 
     return (
         <div>
             <h1>state</h1>
             <h2>컴포넌트 속성값</h2>
-            <h2>{name}</h2>
+            <h2>{name}({age}세)</h2>
             <button onClick={changeName}>Change</button>
         </div>
     );
