bf55099d706203d75baf3f66f9b29ba56398efe1
Seunghun <shlee4508@gmailcom>
2024-03-13 15:04:50 +0900

    state에 따른 업데이트

--- web/React/voca/src/App.js
+++ web/React/voca/src/App.js
@@ -14,8 +14,8 @@ function App() {
   return (
     <div className="App">
       <Hello />
-      <Welcome />
-      <div className={styles.box}>App</div>
+      <Hello />
+      <Hello />
     </div>
   );
 }
--- web/React/voca/src/component/Hello.js
+++ web/React/voca/src/component/Hello.js
@@ -1,19 +1,23 @@
 import { useState } from "react";
 
 export default function Hello() {
-    let name = "Mike"
+    // useState 는 배열을 반환함, 첫번째 값은 state이며 두번째는 state를 변경해주는 함수이다.
+    const [name, setName] = useState("Mike");
+    //let name = "Mike"
     function changeName() {
-        name = name === "Mike" ? "Jane" : "Mike";
+        //name = name === "Mike" ? "Jane" : "Mike";
         // 값은 변경되었지만 dom 업데이트가 되지 않았음
         // dom 이란 컴포넌트와 html을 연결해주는 장치
-        document.getElementById("name").innerText = name;
+        //document.getElementById("name").innerText = name;
+
+        setName(name === "Mike" ? "Jane" : "Mike");
     }
 
     return (
         <div>
             <h1>state</h1>
             <h2>컴포넌트 속성값</h2>
-            <h2 id = "name">{name}</h2>
+            <h2>{name}</h2>
             <button onClick={changeName}>Change</button>
         </div>
     );
